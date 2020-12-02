c9a9cc4482d7372ffb0d9c890efb41bd3b23f594
Stefan Plösser <stefan@rarelime.de>
2020-12-02 18:35:07 +0100

    first rough version

--- src/_app.ts
+++ src/_app.ts
@@ -1,7 +1,32 @@
-const message = 'Typescript ready to rumble!';
+import { readFile, writeFile } from 'fs-extra';
+import { resolve } from 'path';
+import { DIST_DIR, SRC_DIR } from './directories';
 
-function sayHi(msg: string): void {
-  console.log(msg);
+import Mustache = require('mustache');
+
+const defaultEncoding = {
+  encoding: 'utf8',
+};
+
+async function loadTemplate() {
+  const templateFile = resolve(SRC_DIR, 'utag-input.js.mustache');
+  return readFile(templateFile, defaultEncoding);
 }
 
-sayHi(message);
+// Top-level await not possible …
+// eslint-disable-next-line @typescript-eslint/no-floating-promises
+(async function init() {
+  const template = await loadTemplate();
+  const output = Mustache.render(
+    template,
+    {
+      customCss: 'body {  background-color: thistle !important; }',
+      customHtml: '<h1>huhu</h1>',
+      customJs: 'console.log("hello")',
+    },
+    undefined,
+    ['%%%', '%%%'] // opening and closing tags
+  );
+  const targetFile = resolve(DIST_DIR, 'out.js');
+  await writeFile(targetFile, output, defaultEncoding);
+})();
---
+++ src/directories.ts
@@ -0,0 +1,5 @@
+import { resolve } from 'path';
+
+export const BASE_DIR = resolve(__dirname, '..');
+export const SRC_DIR = resolve(BASE_DIR, 'src');
+export const DIST_DIR = resolve(BASE_DIR, 'dist');
